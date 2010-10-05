4b5d89a0532dd1a0754d491f44c43121f06c4da5
Carlos Lopez <carlos_lopez@hms.harvard.edu>
2010-10-05 17:17:08 -0400

    a bit more of the simple example

--- simple_cvode_test.py
+++ simple_cvode_test.py
@@ -2,3 +2,19 @@ from pysundials import cvode
 import ctypes
 from matplotlib import pyplot
 
+# Simple reaction:
+# E + S <-> ES -> E + P
+
+# Parameters
+k1f = 2    #units: of mol^-1 s^-1
+k1r = 1    #units: s^-1
+k1r = 1.5  #units: s^-1
+
+# Initial specs
+S  = 80000
+E  = 40000
+ES = 0
+P  = 0
+
+def R1F(y):
+    return 

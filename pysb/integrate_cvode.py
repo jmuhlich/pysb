d1d6c5b6ac26eb08c3e14492986d93020dd230b8
Carlos Lopez <carlos_lopez@hms.harvard.edu>
2010-10-08 18:08:42 -0400

    just keep swimming...

--- pysb/integrate_cvode.py
+++ pysb/integrate_cvode.py
@@ -6,26 +6,43 @@ import re
 
 
 def odesolve(model, t):
+    # Generate the ODES (Jah sucks at comments)
     pysb.bng.generate_equations(model)
-    # FIXME code outside of model shouldn't have to handle parameter_overrides (same for initial_conditions below)
-    # WTF? WHAT DOES THIS LINE DO????
-    param_subs = dict([ (sympy.Symbol(p.name), p.value) for p in model.parameters + model.parameter_overrides.values() ])
+
+    # Get the size of the ODE array
+    odesize = len(model.odes)
     
+    #make a function that can be passed to cvode
+    #init the arrays we need
+    y = numpy.zeros(len(model.odes))
+    ydot = y.copy() 
+    y0 = y.copy()
+    
+    # assign the initial conditions
+    # FIXME code outside of model shouldn't have to handle parameter_overrides (same for initial_conditions below)
+    # CFL: Species really should be a class with methods such as .name, .index, etc
+    for cplxptrn, ic_parm in model.initial_conditions:
+        override = model.parameter_overrides.get(ic_parm.name)
+        if override is not None:
+            ic_parm = override
+        speci = model.get_species_index(cplxptrn)
+        y0[speci] = ic_parm.value
+    
+    # define function to integrate and assign RHS from BNG
+    def f(t, y, ydot, f_data):
+        for i in 
+    
+
+
     # Get the constants from the model
     c_code_consts = '\n'.join(['float %s = %e;' % (p.name, p.value) for p in model.parameters])
 
 
-    c_code_eqs = '\n'.join(['ydot[%d] = %s;' % (i, sympy.ccode(model.odes[i])) for i in range(len(model.odes))])
+    c_code_eqs = '\n\t'.join(['ydot[%d] = %s;' % (i, sympy.ccode(model.odes[i])) for i in range(len(model.odes))])
     c_code_eqs = re.sub(r's(\d+)', lambda m: 'y[%s]' % (int(m.group(1))), c_code_eqs)
     c_code = c_code_consts + '\n\n' + c_code_eqs
 
     y0 = numpy.zeros((len(model.odes),))
-    for cp, ic_param in model.initial_conditions:
-        override = model.parameter_overrides.get(ic_param.name)
-        if override is not None:
-            ic_param = override
-        si = model.get_species_index(cp)
-        y0[si] = ic_param.value
         
     def rhs(y, t):
         ydot = y.copy()  # seems to be the fastest way to get an array of the same size?

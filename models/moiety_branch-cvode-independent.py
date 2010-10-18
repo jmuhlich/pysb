f0a70e5178ec8c72d0a8d336c3c125d75a5b8268
Carlos Lopez <carlos_lopez@hms.harvard.edu>
2010-10-18 18:34:45 -0400

    added sample files for normal and sensitivity analysis integration

--- models/moiety_branch-cvode-independent.py
+++ models/moiety_branch-cvode-independent.py
@@ -2,3 +2,91 @@ from pysundials import cvode
 import ctypes
 from matplotlib import pyplot
 
+V1 = 1.0
+V2 = 10.0
+V3 = 1.0
+V4 = 1.0
+K1_X0 = 1.0
+
+K1_S2 = 1.0
+K2_S3 = 1.0
+K2_X6 = 1.0
+K3_S1 = 1.0
+K4_S1 = 1.0
+X0 = 1.0
+X4 = 1.0e-12
+X5 = 1.0e-12
+X6 = 1.0
+X7 = 1.0e-12
+
+S1 = 0
+S2 = 1
+#S3 = 2
+
+def R1(y):
+    return (V1 /( K1_X0 * K1_S2 )*( X0*y[S2 ]))/ \
+        (1 + X0/ K1_X0 + y[S2 ]/ K1_S2 +\
+             (X0*y[S2 ])/( K1_X0 * K1_S2 ))
+
+def R2(y):
+    return (V2 /( K2_S3 * K2_X6 ))*(( icsum -y[S2 ])* X6 )/\
+        (1 + (icsum -y[S2 ])/ K2_S3 + X6/ K2_X6 \
+             + ((( icsum -y[S2 ])* X6 )/( K2_S3 * K2_X6 )))
+ 
+def R3(y):
+    return (V3*y[S1 ])/( y[S1] + K3_S1 )
+
+def R4(y):
+    return (V4*y[S1 ])/( y[S1] + K4_S1 )
+ 
+def f(t, y, ydot , f_data ):
+    ydot [S2] = R2(y) - R1(y)
+    ydot [S1] = R1(y) - R3(y) - R4(y)
+    return 0
+
+y = cvode . NVector ([1.0 , 0.7])
+icsum = 0.7 + 0.3 # setting the initial sum of the conservation
+
+cvode_mem = cvode.CVodeCreate(cvode.CV_BDF, cvode.CV_NEWTON)
+cvode.CVodeMalloc(cvode_mem, f, 0.0, y, cvode.CV_SS, 1.0e-8, 1.0e-12)
+cvode.CVDense ( cvode_mem , 2)
+
+results = ([] , [], [], [])
+
+t = cvode . realtype (0.0)
+results [0]. append (t.value)
+results [1]. append (y[S1])
+results [2]. append (y[S2])
+results [3]. append ((icsum- y[S2]))
+
+iout = 1
+tout = 0.1
+while iout <= 50:
+    ret = cvode.CVode(
+        cvode_mem,
+        tout,
+        y,
+        ctypes.byref (t),
+        cvode.CV_NORMAL
+        )
+    if ret != 0:
+        print " CVODE Error: %i"%(ret)
+        break
+    results[0].append(tout)
+    results[1].append(y[S1])
+    results[2].append(y[S2])
+    results[3].append((icsum -y[S2]))
+     
+    iout += 1
+    tout += 0.1
+
+pyplot.title("PySUNDIALS/CVODE")
+pyplot.xlabel("t")
+pyplot.ylabel("Concentration")
+pyplot.plot(
+    results[0], results[1], "k-",
+    results[0], results[2], "k--",
+    results[0], results[3], "k-."
+    )
+pyplot.legend(("$S_1$", "$S_2$", "$S_3$"))
+pyplot.show()
---
+++ models/moiety_branch-cvodes-independent.py
@@ -0,0 +1,151 @@
+from pysundials import cvodes
+from pysundials import nvecserial
+import ctypes
+from matplotlib import pyplot
+
+#V1 = 1.0
+#V2 = 10.0
+#V3 = 1.0
+#V4 = 1.0
+
+
+K1_X0 = 1.0
+K1_S2 = 1.0
+K2_S3 = 1.0
+K2_X6 = 1.0
+K3_S1 = 1.0
+K4_S1 = 1.0
+
+X0 = 1.0
+X4 = 1.0e-12
+X5 = 1.0e-12
+X6 = 1.0
+X7 = 1.0e-12
+
+S1 = 0
+S2 = 1
+
+V1 = 0
+V2 = 1
+V3 = 2
+V4 = 3
+
+class UserData (ctypes.Structure):
+    _fields_ = [("p", cvodes.realtype*4)]
+
+PUserData = ctypes.POINTER(UserData)
+
+def R1(y, p):
+    return (p[V1] /( K1_X0 * K1_S2 )*( X0*y[S2])) / \
+        (1 + X0/ K1_X0 + y[S2]/ K1_S2 + (X0*y[S2])/( K1_X0 * K1_S2 ))
+
+def R2(y, p):
+    return (p[V2] /( K2_S3 * K2_X6 ))*(( icsum -y[S2])* X6 )/\
+        (1 + (icsum -y[S2])/ K2_S3 + X6/ K2_X6 \
+             + ((( icsum - y[S2])* X6)/( K2_S3 * K2_X6 )))
+ 
+def R3(y, p):
+    return (p[V3]*y[S1])/(y[S1] + K3_S1)
+
+def R4(y, p):
+    return (p[V4]*y[S1])/(y[S1] + K4_S1)
+ 
+def f(t, y, ydot, f_data):
+    data = ctypes.cast(f_data, PUserData).contents
+
+    ydot [S2] = R2(y, data.p) - R1(y, data.p)
+    ydot [S1] = R1(y, data.p) - R3(y, data.p) - R4(y, data.p)
+    return 0
+
+data = UserData()
+data.p[V1] = 1.0
+data.p[V2] = 10.0
+data.p[V3] = 1.0
+data.p[V4] = 1.0
+
+y = cvodes.NVector([1.0 , 0.7])
+icsum = 0.7 + 0.3 # setting the initial sum of the conservation
+
+cvodes_mem = cvodes.CVodeCreate(cvodes.CV_BDF, cvodes.CV_NEWTON)
+cvodes.CVodeMalloc(cvodes_mem, f, 0.0, y, cvodes.CV_SS, 1.0e-8, 1.0e-12)
+cvodes.CVodeSetFdata(cvodes_mem, ctypes.pointer(data))
+cvodes.CVDense (cvodes_mem, 2)
+
+yS = nvecserial.NVectorArray([([0]*2)]*4)
+
+cvodes.CVodeSensMalloc(cvodes_mem, 4, cvodes.CV_SIMULTANEOUS, yS)
+
+# So this bit below might seem like magic , but it's fairly straight
+# forward. This particular line doesn't look like the example
+# cvsfwddenx.py because we are not suplying our own sensitivity
+# function.
+
+# CVodeSetSensParams expects four parameters
+# (for more detail see p. 111 of the CVODES user guide)
+# 1. the cvodes memory object
+# 2. a pointer to the array of parameter values which MUST be passed
+#    through the user data structure (so CVODES knows where the values
+#    are and can peturb them, presumably)
+# 3. an array (i.e. list) of scaling factors, one for each parameter
+#    for which sensitivies are to be determined
+# 4. an array of integers (either 1 or 0) , where a 1 indicates the
+#    respective parameter value should be used in estimating
+#    sensitivities
+
+cvodes.CVodeSetSensParams(cvodes_mem,
+                          data.p, # we have four system parameters ( The four VMax's)
+                          [1]*4,  # they should all be scaled by 1, i.e. unscaled,
+                          [1]*4   # they all contribute to the estimation of sensitivities
+                          )
+
+results = ([], [], [], [], [], [])
+
+t = cvodes.realtype (0.0)
+results[0].append(t.value)
+results[1].append(y[S1])
+results[2].append(y[S2])
+results[3].append((icsum- y[S2]))
+results[4].append(yS[V2][S1])
+results[5].append(yS[V2][S2])
+
+iout = 1
+tout = 0.05
+while iout <= 80:
+    ret = cvodes.CVode(cvodes_mem,
+                       tout,
+                       y,
+                       ctypes.byref (t),
+                       cvodes.CV_NORMAL
+                       )
+    cvodes.CVodeGetSens(cvodes_mem, t, yS)
+    if ret != 0:
+        print " CVODE Error: %i"%(ret)
+        break
+    results[0].append(tout)
+    results[1].append(y[S1])
+    results[2].append(y[S2])
+    results[3].append((icsum -y[S2]))
+    results[4].append(yS[V2][S1])
+    results[5].append(yS[V2][S2])
+
+    iout += 1
+    tout += 0.1
+
+pyplot.figure(1)
+pyplot.subplot(211)
+pyplot.ylabel("Concentration")
+pyplot.plot(
+    results[0], results[1], "k-",
+    results[0], results[2], "k--",
+    results[0], results[3], "k-."
+    )
+pyplot.legend(("$S_1$", "$S_2$", "$S_3$"))
+
+pyplot.subplot (212)
+pyplot.xlabel ("t")
+pyplot.plot(
+    results[0], results[4], "k-",
+    results[0], results[5], "k--"
+    )
+pyplot.legend(("$\delta S_1/dV_2$", "$\delta S_2/dV_2$"))
+pyplot.show()

3f9fcd38cae49fcd81db2e3ab0938b4ea6e96567
clopez <clopez@morpheus.(none)>
2011-04-01 12:02:59 -0400

    made some changes, almost done with anneal_sundials.py

---
+++ models/anneal_test.py
@@ -0,0 +1,7 @@
+# from the Landau spinodal theory:
+# Define
+# V(x) = (1/2)rx^2 + (1/4)ux^4
+# r>0: parabola with minimum at x=0
+# r<0: double well potential, where x=0 is the local maximum (the
+#      barrier between the wells) 
+#      the well locations are at + or - sqrt(|r|/u)
--- pysb/anneal_sundials.py
+++ pysb/anneal_sundials.py
@@ -1,19 +1,14 @@
 import pysb.bng
-import numpy, sympy, re, ctypes, sys
-from pysundials import cvode, cvodes, nvecserial
+import numpy, sympy, re, ctypes
+import csv
+from pysundials import cvode
 
-#FIXME: add a odesolvesensedky function or add this function to odesenssolve
+# These set of functions set up the system for annealing runs
+# and provide the runner function as input to annealing
 
-def odeinit(model):
-
-    #Generate ODES from BNG
-    # import code
-    # code.interact(local=locals())
-    # import pdb
-    # pdb.set_trace()
-
+def annealinit(model, reltol=1.0e-7, abstol=1.0e-11, nsteps = 100):
+    # Generate equations
     pysb.bng.generate_equations(model)
-
     # Get the size of the ODE array
     odesize = len(model.odes)
     
@@ -35,8 +30,6 @@ def odeinit(model):
     y = cvode.NVector(yzero)
     numparams = len(model.parameters)
         
-    #print "initial parameter values:\n", y
-
     # make a dict of ydot functions. notice the functions are in this namespace.
     # replace the kxxxx constants with elements from the params array
     rhs_exprs = []
@@ -72,46 +65,44 @@ def odeinit(model):
         for i in range(0,len(model.odes)):
             ydot[i] = eval(rhs_exprs[i], rhs_locals)
         return 0
-
-    return f, rhs_exprs, y, ydot, odesize, data
-
-def odesolve(model, tfinal, nsteps = 100, tinit = 0.0, reltol=1.0e-7, abstol=1.0e-11):
-    '''
-    model: a model object from pysb
-    tfinal: the final time to run the integration (in units of pysb model, usually second)
-    _calspec: passed from calibrate_anneal to know which observable to use as calibration ref
-    nsteps: the number of steps to take between time reports, default = 100
-    tinit: the initial time-step, default = 0.0
-    reltol: relative tolerance for sundials, default=1.0e-7
-    abstol: absolute tolerance for sundials, default=1.0e-11
-    '''
-    tadd = tfinal/nsteps
-
-    SOMEFLAG = True
-    if SOMEFLAG:
-        f, rhs_exprs, y, ydot, odesize, data = odeinit(model)
-
-    # initialize the cvode memory object
+    
+    # CVODE STUFF
+    # initialize the cvode memory object, use BDF and Newton for stiff
     cvode_mem = cvode.CVodeCreate(cvode.CV_BDF, cvode.CV_NEWTON)
-    
-    # allocate the cvode memory as needed
+    # allocate the cvode memory as needed, pass the function and the init ys
     cvode.CVodeMalloc(cvode_mem, f, 0.0, y, cvode.CV_SS, reltol, abstol)
-    
     # point the parameters to the correct array
     cvode.CVodeSetFdata(cvode_mem, ctypes.pointer(data))
-
     # link integrator with linear solver
     cvode.CVDense(cvode_mem, odesize)
-    
+
     #list of outputs
     xout = numpy.zeros(nsteps)
     yout = numpy.zeros([nsteps, odesize])
 
     #initialize the arrays
     print "Initial parameter values:", y
-    xout[0] = tinit
+    xout[0] = 0.0 #CHANGE IF NEEDED
+    #first step in yout
     for i in range(0, odesize):
         yout[0][i] = y[i]
+
+    return [f, rhs_exprs, y, ydot, odesize, data, xout, yout, nsteps, cvode_mem]
+
+
+def annealodesolve(model, tfinal, initlist, tinit = 0.0):
+    f = initlist[0]
+    rhs_exprs = initlist[1]
+    y = initlist[2]
+    ydot = initlist[3]
+    odesize = initlist[4]
+    data = initlist[5]
+    xout = initlist[6]
+    yout = initlist[7]
+    nsteps = initlist[8]
+    cvode_mem = initlist[9]
+
+    tadd = tfinal/nsteps
 
     t = cvode.realtype(tinit)
     tout = tinit + tadd
@@ -146,114 +137,6 @@ def odesolve(model, tfinal, nsteps = 100, tinit = 0.0, reltol=1.0e-7, abstol=1.0
     yobs.T
     return (xout,yobs,yout)
 
-def odesenssolve(model, tfinal, nsteps = 100, tinit = 0.0, senslist=None,
-                 sensmaglist=None, reltol=1.0e-7, abstol=1.0e-11):
-    tadd = tfinal/nsteps
-
-    SOMEFLAG = True
-    if SOMEFLAG:
-        f, rhs_exprs, y, ydot, odesize, data = odeinit(model, senslist)
-
-    # initialize the cvode memory object
-    cvodes_mem = cvodes.CVodeCreate(cvodes.CV_BDF, cvodes.CV_NEWTON)
-
-    if senslist is None:
-        #make a senslist for all parameters
-        senslist = [n for n in range(0, len(model.parameters))]
-
-    numsens = len(senslist)
-
-    # the sensitivity function needs an array of the scaling factors for each parameter
-    # for which sensitivity will be calculated
-    # default a scale of "1" unless sensmaglist is passed
-    if sensmaglist is None and senslist is None:
-        sensmaglist = [1 for n in range(0, numsens)]
-    elif sensmaglist is None and senslist is not None:
-        #senslist was passed, assign mags of 1 to the items in senslist, 0 otherwise
-        sensmaglist = [0 for n in range(0, numsens)]
-        for n in senslist:
-            sensmaglist[n] = 1
-    else:
-        print "something is really wrong with the SENSLIST or SENSMAGLIST"
-
-    #FIXME: Should we remove the initial value sensitivities automatically?
-
-    # set the sensitivity array
-    yS = nvecserial.NVectorArray([([0]*odesize)]*numsens)
-
-    # CVodeSensMalloc allocates and initializes memory for sensitivity computations
-    cvodes.CVodeSensMalloc(cvodes_mem, numsens, cvodes.CV_STAGGERED, yS)
-
-    # CVodeSetSensParams sets the parameters for the sensitivity function call
-    print "SENSLIST:", senslist
-    print "SENSMAGLIST:", sensmaglist
-
-    cvodes.CVodeSetSensParams(cvodes_mem, data.p,
-                              sensmaglist,
-                              senslist)
-
-    # point the user parameters to the correct array
-    cvodes.CVodeSetFdata(cvodes_mem, ctypes.pointer(data))
-    
-    # allocate the cvodes memory as needed
-    cvodes.CVodeMalloc(cvodes_mem, f, 0.0, y, cvodes.CV_SS, reltol, abstol)
-    
-    # link integrator with linear solver
-    cvodes.CVDense(cvodes_mem, odesize)
-    
-    #list of outputs
-    yout = numpy.zeros([nsteps, odesize])
-    xout = numpy.zeros(nsteps)
-    ysensout = numpy.zeros([odesize, nsteps, numsens])
-
-    #initialize the arrays
-    print "Initial parameter values:", y
-    xout[0] = tinit
-    for i in range(0, odesize):
-        yout[0][i] = y[i]
-
-    for n in range(0, numsens):
-        for o in range(0, odesize):
-            ysensout[i] = yS[n][o]
-
-    t = cvode.realtype(tinit)
-    tout = tinit + tadd
-
-    print "Beginning integration, TINIT:", tinit, "TFINAL:", tfinal, "TADD:", tadd, "ODESIZE:", odesize
-    for step in range(1, nsteps):
-        ret = cvodes.CVode(cvodes_mem, tout, y, ctypes.byref(t), cvodes.CV_NORMAL)
-        cvodes.CVodeGetSens(cvodes_mem, t, yS)
-        
-        if ret !=0:
-            print "CVODES ERROR %i"%(ret)
-            break
-
-        xout[step]= tout
-        for i in range(0, odesize):
-            yout[step][i] = y[i]
-            for j in range(0, numsens):
-                #import code
-                #code.interact(local=locals())
-                ysensout[i][step][j] = yS[j][i] # yS[numsens][odesize]
-
-            
-        # increase the time counter
-        tout += tadd
-    print "Integration finished"
-
-    #now deal with observables
-    obs_names = [name for name, rp in model.observable_patterns]
-    yobs = numpy.zeros([len(obs_names), nsteps])
-    
-    #sum up the correct entities
-    for i, name in enumerate(obs_names):
-        factors, species = zip(*model.observable_groups[name])
-        yobs[0] = (yout[:, species] * factors).sum(1)
-
-    #transpose yobs to make it easy to plot
-    yobs.T
-    return (xout, yobs, yout, ysensout)
-
 def read_csv_array(fp):
     """returns the first string and a numpy array from a csv set of data"""
     reader = csv.reader(fp)
@@ -264,11 +147,11 @@ def read_csv_array(fp):
     #remove empty spaces
     for i in range(0, len(templist)):
         templist[i] = filter(None, templist[i])
-    #Now put these into a numpy array
-    converters = (float, float, float, float)
     headstring = templist.pop(0)
+    #Now put these into a numpy array, assume they are all floats
+    converters = tuple([float]*len(templist[0]))
     #assume all entries in the list are the same length
-    darray = numpy.zeros((len(templist), len(templist[0])))
+    darray = numpy.zeros((len(templist[0]), len(templist)))
     for i, item in enumerate(templist):
         darray[i] = numpy.asarray(templist[i], dtype=darray.dtype)    
     return (headstring, darray)
@@ -330,24 +213,3 @@ def compare_data(array0, array1, array0var=None, obspec=None):
     
     return objarray.sum()
     
-def odeanneal(refarray, params, model, tfinal, obspec=0):
-    '''
-    paramarray: array of parameters, from the model
-    eqns: dict of ode's
-    time: time to run the simulation
-    '''
-    # This is the feeder function for the anneal routine. It needs to:
-    # 1- run the integration for the model with parameters as vars
-    # 2- calculate the objective function and return it
-    # 3- return the difference
-    
-    #FIXME: something about params here???? Say which params are being passed on by the fxn
-
-
-    # output contains xout, yobs, yout
-    output = odesolve(model, tfinal)
-    # specify which output is being compared!!
-    
-    arrdiff = compare_data(refarray, output[obspec], )
-    
-    return diff
